583e6238f9fc2a73d6c917578668138041c1f1e1
Stuart <lodge.stuart@gmail.com>
2013-05-04 07:35:03 +0100

    nuget

---
+++ packages/MvvmCross.HotTuna.StarterPack.3.0.3/content/MonoAndroid16/Views/FirstView.cs
@@ -0,0 +1,18 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using Cirrious.MvvmCross.Droid.Views;
+
+namespace YourNamespace.Droid.Views
+{
+    [Activity(Label = "View for FirstViewModel")]
+    public class FirstView : MvxActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.FirstView);
+        }
+    }
+}
